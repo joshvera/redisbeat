91f9799cabf0792eefc37d50396c4b9ea46705de
Josh Vera <josh@joshvera.com>
2023-04-16 21:28:54 -0400

    Set sync_every argument from kwargs

--- redisbeat/scheduler.py
+++ redisbeat/scheduler.py
@@ -47,6 +47,7 @@ class RedisScheduler(Scheduler):
         else:
             self.rdb = StrictRedis.from_url(self.schedule_url)
         Scheduler.__init__(self, *args, **kwargs)
+        self.sync_every = kwargs['sync_every']
         app.add_task = partial(self.add, self)
 
         self.multi_node = app.conf.get("redis_multi_node_mode", False)
